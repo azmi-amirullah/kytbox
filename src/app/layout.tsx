744fb96c1ff12775572537094f896c3c6441e2e7
Mohd. Azmi Amirullah. A <azmiupb@gmail.com>
2026-01-24 13:27:05 +0700

    feat: add global top loading bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import NextTopLoader from 'nextjs-toploader';
 import { ThemeProvider } from '@/components/theme-provider';
 import { ToastProvider } from '@/components/toast-provider';
 import { Analytics } from '@vercel/analytics/next';
@@ -37,6 +38,17 @@ export default function RootLayout({
           enableSystem={false}
           disableTransitionOnChange
         >
+          <NextTopLoader
+            color='var(--primary)'
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={false}
+            easing='ease'
+            speed={200}
+            shadow='0 0 10px var(--primary),0 0 5px var(--primary)'
+          />
           {children}
           <ToastProvider />
         </ThemeProvider>
