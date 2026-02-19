141aa341a3a09ad85fdecdd84f0468d4db0bd2ad
Mohd. Azmi Amirullah. A <azmiupb@gmail.com>
2026-02-19 18:41:00 +0700

    fix:  type mismatch

--- src/app/(platform)/bio/loading.tsx
+++ src/app/(platform)/bio/loading.tsx
@@ -1,12 +1,11 @@
-import DashboardClient from './components/DashboardClient';
-import type { Database } from '@/types/supabase';
+import DashboardClient, { Profile } from './components/DashboardClient';
 
 export default function Loading() {
   return (
     <div className='max-w-7xl mx-auto px-3 sm:px-4 py-4 md:py-8 w-full'>
       <DashboardClient
         initialLinks={[]}
-        profile={{} as Database['public']['Tables']['profiles']['Row']}
+        profile={{} as Profile}
         publicUrl=''
         totalViews={0}
         isLoading={true}
