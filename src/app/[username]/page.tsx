6b936dd72bec6d2258a57a0e82669c67fb30e7fe
Mohd. Azmi Amirullah. A <azmiupb@gmail.com>
2026-07-24 09:09:10 +0700

    perf(bio): add high-priority avatar preload link to public profile page

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -24,6 +24,14 @@ export default async function PublicProfilePage({
 
   return (
     <div className='flex flex-col min-h-screen'>
+      {data.profile.avatar_url && (
+        <link
+          rel='preload'
+          as='image'
+          href={data.profile.avatar_url}
+          fetchPriority='high'
+        />
+      )}
       <ProfileView
         profile={data.profile}
         links={data.links}
